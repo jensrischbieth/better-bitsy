d5af6666cd590e897f7920887597cea2810b421b
Tanvir Shahriar <tshahria@fb.com>
2022-10-13 13:18:08 -0400

    cleanup

--- editor/script/util.js
+++
@@ -1,8 +0,0 @@
-/* UTILS
-miscellaneous utility functions for the editor
-TODO: encapsulate in an object maybe? or is that overkill?
-*/
-
-function clamp(val, min, max) {
-	return Math.max(Math.min(val, max), min);
-}
